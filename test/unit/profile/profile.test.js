bd859a0fb3d6b77e603d82e5b74f5f252bb33903
zeppi <jessopb@gmail.com>
2024-06-25 08:24:58 -0400

    profile v001 tests should check assigns, v000 cleanup

--- test/unit/profile/profile.test.js
+++ test/unit/profile/profile.test.js
@@ -4,7 +4,7 @@ import { SendFactory } from '../../../utils/aos.helper.js'
 import fs from 'node:fs'
 import {findMessageByTag, findMessageByTagValue, logSendResult} from "../../../utils/message.js";
 
-const REGISTRY = 'kFYMezhjcPCZLr2EkkwzIXP5A64QmtME6Bxa8bGmbzI'
+const REGISTRY = 'dWdBohXUJ22rfb8sSChdFh6oXJzbAtGe4tC6__52Zk4';
 const PROFILE_A_ID = "PROFILE_A_ID";
 const AUTHORIZED_ADDRESS_A = "ADDRESS_A";
 const AUTHORIZED_ADDRESS_B = "ADDRESS_B";
@@ -15,32 +15,35 @@ function getTag(msg, tagName) {
 
 test('load source profile', async () => {
     const code = fs.readFileSync('./src/ao-profile/profile.lua', 'utf-8')
-    const result = await Send({ Owner: AUTHORIZED_ADDRESS_A, From: AUTHORIZED_ADDRESS_A, Action: "Eval", Data: code })
+    const result = await Send({ Owner: AUTHORIZED_ADDRESS_A, From: AUTHORIZED_ADDRESS_A, ProfileVersion: '0.0.1', Action: "Eval", Data: code })
 })
+test("------------------------------P V001 BEGIN TEST------------------------------")
 
 test('should fail to update if from is not owner', async () => {
-    const updateResult = await Send({ Id: "1111", From: AUTHORIZED_ADDRESS_B, Action: "Update-Profile", Data: JSON.stringify({ UserName: "Steve", DisplayName: "Steverino" }) })
+    const updateResult = await Send({ Id: "1111", ProfileVersion: '0.0.1', From: AUTHORIZED_ADDRESS_B, Action: "Update-Profile", Data: JSON.stringify({ UserName: "Steve", DisplayName: "Steverino" }) })
     logSendResult(updateResult, "Update-Profile--Fail")
     // const registryMessages = findMessageByTarget(updateResult.Messages, REGISTRY)
     const statusMessages = findMessageByTag(updateResult.Messages, "Status");
     assert.equal(getTag(statusMessages[0], "Status"), "Error")
 })
 test('should update with data', async () => {
-    const updateResult = await Send({ Id: "1112", From: AUTHORIZED_ADDRESS_A, Action: "Update-Profile", Data: JSON.stringify({ UserName: "Steve", DisplayName: "Steverino" }) })
+    const updateResult = await Send({ Id: "1112", ProfileVersion: '0.0.1', From: AUTHORIZED_ADDRESS_A, Action: "Update-Profile", Data: JSON.stringify({ UserName: "Steve", DisplayName: "Steverino" }) })
     logSendResult(updateResult, "Update-Profile--Pass")
     const statusMessages = findMessageByTag(updateResult.Messages, "Status");
     assert.equal(getTag(statusMessages[0], "Status"), "Success")
+    assert.equal(updateResult.Assignments[0].Message, "1112")
 })
 
 test('should update with tags', async () => {
-    const updateResult = await Send({ Id: "1113", From: AUTHORIZED_ADDRESS_A, Action: "Update-Profile", DisplayName: "El Steverino" })
+    const updateResult = await Send({ Id: "1113", From: AUTHORIZED_ADDRESS_A, ProfileVersion: '0.0.1', Action: "Update-Profile", DisplayName: "El Steverino" })
     logSendResult(updateResult, "Update-Profile")
     const statusMessages = findMessageByTag(updateResult.Messages, "Status");
     assert.equal(getTag(statusMessages[0], "Status"), "Success")
+    assert.equal(updateResult.Assignments[0].Message, "1113")
 })
 
 test('should get info', async () => {
-    const info = await Send({ Action: "Info" })
+    const info = await Send({ Action: "Info", ProfileVersion: '0.0.1' })
     logSendResult(info, "Info")
     const dataMessage = findMessageByTagValue(info.Messages, "Action", "Read-Success");
     const dataString = dataMessage[0]?.Data;
--- test/unit/profile/profile_v000.test.js
+++ test/unit/profile/profile_v000.test.js
@@ -12,7 +12,7 @@ const {Send} = SendFactory({ processId: PROFILE_A_ID, moduleId: '5555'});
 function getTag(msg, tagName) {
     return msg?.Tags?.find(t => t.name === tagName)?.value ?? null;
 }
-
+test("------------------------------P V000 BEGIN TEST------------------------------")
 test('load source profile', async () => {
     const code = fs.readFileSync('./src/ao-profile/profile.lua', 'utf-8')
     const result = await Send({ Owner: AUTHORIZED_ADDRESS_A, From: AUTHORIZED_ADDRESS_A, Action: "Eval", Data: code })
@@ -20,28 +20,28 @@ test('load source profile', async () => {
 
 test('should fail to update if from is not owner', async () => {
     const updateResult = await Send({ Id: "1111", From: AUTHORIZED_ADDRESS_B, Action: "Update-Profile", Data: JSON.stringify({ UserName: "Steve", DisplayName: "Steverino" }) })
-    logSendResult(updateResult, "Update-Profile--Fail")
+    // logSendResult(updateResult, "Update-Profile--Fail")
     // const registryMessages = findMessageByTarget(updateResult.Messages, REGISTRY)
     const statusMessages = findMessageByTag(updateResult.Messages, "Status");
     assert.equal(getTag(statusMessages[0], "Status"), "Error")
 })
 test('should update with data', async () => {
     const updateResult = await Send({ Id: "1112", From: AUTHORIZED_ADDRESS_A, Action: "Update-Profile", Data: JSON.stringify({ UserName: "Steve", DisplayName: "Steverino" }) })
-    logSendResult(updateResult, "Update-Profile--Pass")
+    // logSendResult(updateResult, "Update-Profile--Pass")
     const statusMessages = findMessageByTag(updateResult.Messages, "Status");
     assert.equal(getTag(statusMessages[0], "Status"), "Success")
 })
 
 test('should update with tags', async () => {
     const updateResult = await Send({ Id: "1113", From: AUTHORIZED_ADDRESS_A, Action: "Update-Profile", DisplayName: "El Steverino" })
-    logSendResult(updateResult, "Update-Profile")
+    // logSendResult(updateResult, "Update-Profile")
     const statusMessages = findMessageByTag(updateResult.Messages, "Status");
     assert.equal(getTag(statusMessages[0], "Status"), "Success")
 })
 
 test('should get info', async () => {
     const info = await Send({ Action: "Info" })
-    logSendResult(info, "Info")
+    // logSendResult(info, "Info")
     const dataMessage = findMessageByTagValue(info.Messages, "Action", "Read-Success");
     const dataString = dataMessage[0]?.Data;
     const data = JSON.parse(dataString);
