5a9efbeee5f036c18753de2f491524307edb72a2
jessopb <36554050+jessopb@users.noreply.github.com>
2024-09-05 16:31:22 -0400

    api wip changes (#43)
    * api wip changes
    
    * cleanup
    
    * fix module resolution with tsc-alias for now, maybe bundle later
    
    * cleanup

--- api/src/types/profile.ts
+++ api/src/types/profile.ts
@@ -1,3 +1,4 @@
 export type ReadProfileArgs = {
 	processId: string;
+	keys?: string[]
 };
--- api/src/types/zone.ts
+++ api/src/types/zone.ts
@@ -1,6 +1,9 @@
-export type ReadProfileArgs = {
-	processId: string;
-	keys: string[];
-};
+import {TagType} from "types/helpers";
 
-export type APICreateZone = {};
+export type APICreateZone = {
+	module: string;
+	scheduler: string;
+	wallet: string;
+	tags: TagType[];
+	data: any;
+};
